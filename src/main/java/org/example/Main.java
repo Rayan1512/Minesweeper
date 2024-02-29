b785a644ac056078b82daf9c864afb271f8b492b
Rayyan Ahmed <rayyanahmed99@gmail.com>
2024-02-29 10:27:19 +0000

    tester commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Minesweeper");
-        
+        System.out.printf("Welcome to Minesweeper");
+        System.out.printf("Get ready!");
 
     }
 }

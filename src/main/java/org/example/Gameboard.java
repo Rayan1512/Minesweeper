0535f19e0d36c417ba3aace44d9f891b61cfb863
Rayyan1512 <rayyanahmed99@gmail.com>
2024-03-01 10:16:20 +0000

    Game board 1

--- src/main/java/org/example/Gameboard.java
+++ src/main/java/org/example/Gameboard.java
@@ -3,58 +3,24 @@ package org.example;
 import java.util.Scanner;
 import java.util.Random;
 import java.util.List;
-public class minesweeper {
-    public static final int Size = 10;
-    public static final int Mines = 10;
+public class Gameboard {
+    public static void main(String[] args) {
+        int gridSize = 10;
+        int[][] grid = new int[gridSize][gridSize];
 
-    public char[][] board;
-    public boolean[][] mines;
-    public boolean[][] revealed;
-
-    public minesweeper() {
-        board = new char[Size][Size];
-        mines = new boolean[Size][Size];
-        revealed = new boolean[Size][Size];
-        initialiseBoard();
-        mineSelect();
-
-    }
-
-    public void initializeBoard() {
-        for (int i = 0; i < Size; i++) {
-            for (int j = 0; j < Size; j++) {
-                board[i][j] = '-';
+        // Initialize the grid with some values (for demonstration purposes)
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                grid[i][j] = i * gridSize + j;
             }
         }
-    }
 
-
-    public void placeMines() {
-        for (int i = 0; i < Mines; i++) {
-            int row, col;
-            do {
-                row = (int) (Math.random() * Size);
-                col = (int) (Math.random() * Size);
-            } while (mines[row][col]);
-
-            mines[row][col] = true;
-        }
-
-
-        public static void displayBoard () {
-            for (int i = 0; i < Size; i++) {
-                for (int j = 0; j < Size; j++) {
-                    if (revealed[i][j]) {
-                        System.out.print(board[i][j] + " ");
-                    } else {
-                        System.out.print("- ");
-                    }
-                }
-                System.out.println();
+        // Display the grid
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                System.out.print("[ ]");
             }
-
-
+            System.out.println();
         }
-
     }
 }

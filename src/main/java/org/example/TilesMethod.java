0f45049c90b12076da9416523118a8068577668d
Rayyan1512 <rayyanahmed99@gmail.com>
2024-03-04 09:13:27 +0000

    Game board ready

--- src/main/java/org/example/TilesMethod.java
+++ src/main/java/org/example/TilesMethod.java
@@ -1,6 +1,12 @@
-package org.example;
+//package org.example;
+//public class TilesMethod {
+ //   public static final int SIZE = 10;
+   // public static int[][] grid; // Declare the array as a class variable
 
-public class TilesMethod {
-    //boolean bomb = new boolean[];
+    //public static void main(String[] args) {
+      //  grid = new int[SIZE][SIZE]; // Initialize the array
 
-}
+    //}
+
+
+//}
